378371174d70a47f148ec46fa777d23c5c2da7af
Mario Andrés Correa <a@macool.me>
2017-05-12 01:28:29 -0500

    show taggings count

--- app/admin/hashtags.rb
+++ app/admin/hashtags.rb
@@ -1,3 +1,11 @@
 ActiveAdmin.register ActsAsTaggableOn::Tag, as: 'Hashtags' do
   permit_params :name
+
+  index do
+    selectable_column
+    column :id
+    column :name
+    column :taggings_count
+    actions
+  end
 end
