442cefaf140c471dbffa4e3597b1066a976a2c30
Mario Andrés Correa <a@macool.me>
2017-05-12 21:18:51 -0500

    bootstrap results

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -4,6 +4,7 @@ class ResultsController < ApplicationController
     @subentidad = @entidad.subentidades.find params[:status_id]
     @estado = @subentidad.estados.find params[:situation_id]
     @situacion = @estado.situaciones.find params[:id]
-    @resultados = [] # TODO
+    search_results = Resultado.tagged_with(@situacion.tags).to_a
+    @resultados = search_results.to_a.sample(4)
   end
 end
