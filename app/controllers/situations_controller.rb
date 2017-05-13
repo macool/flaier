c212452fcff1c5ec74df4a6a4dfda926422384c0
Mario Andrés Correa <a@macool.me>
2017-05-12 21:06:00 -0500

    jump empty situaciones

--- app/controllers/situations_controller.rb
+++ app/controllers/situations_controller.rb
@@ -4,5 +4,23 @@ class SituationsController < ApplicationController
     @subentidad = @entidad.subentidades.find params[:status_id]
     @estado = @subentidad.estados.find params[:id]
     @situaciones = @estado.situaciones
+
+    if @situaciones.count == 1
+      # edge case: N
+      # only one situacion
+      redirect_to situacion_button_link(@situaciones.first)
+    end
   end
+
+  private
+
+  def situacion_button_link(situacion)
+    survey_status_situation_result_path(
+      survey_id: @entidad.id,
+      status_id: @subentidad.id,
+      situation_id: @estado.id,
+      id: situacion.id
+    )
+  end
+  helper_method :situacion_button_link
 end
